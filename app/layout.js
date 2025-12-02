998f94bc66dca50013bd5561a149aea0305dde5a
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2025-12-02 08:32:07 +0530

    Added Analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
         <SpeedInsights />
+        <Analytics />
         <Toaster position="top-right" richColors />
       </body>
     </html>
